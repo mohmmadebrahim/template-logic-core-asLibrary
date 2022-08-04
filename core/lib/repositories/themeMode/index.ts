8261cc7161f1c0d8c0ade31bc2f4a101415b6ccc
Mohammad Ebrahim GHobadi <m.e.ghobadi.76@gmail.com>
2022-08-04 12:35:39 +0430

    disable auto theme change by browser

--- core/lib/repositories/themeMode/index.ts
+++ core/lib/repositories/themeMode/index.ts
@@ -4,14 +4,14 @@ import { IThemeModeProps, ThemeModeSetUp } from "../../util"
 export const ThemeMode = async ({ mode }: Partial<IThemeModeProps>) => {
     mode && ThemeModeSetUp({ mode: mode })
     mode && await localforage.setItem("theme-mode", mode)
-    const localMode = await localforage.getItem("theme-mode")
+    // const localMode = await localforage.getItem("theme-mode")
 
-    if (!localMode) {
-        // On page load or when changing themes, best to add inline in `head` to avoid FOUC
-        if (localMode === 'dark' || window.matchMedia('(prefers-color-scheme: dark)').matches) {
-            ThemeModeSetUp({ mode: "dark" })
-        } else {
-            ThemeModeSetUp({ mode: "light" })
-        }
-    }
+    // if (!localMode) {
+    //     // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+    //     if (localMode === 'dark' || window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    //         ThemeModeSetUp({ mode: "dark" })
+    //     } else {
+    //         ThemeModeSetUp({ mode: "light" })
+    //     }
+    // }
 }
